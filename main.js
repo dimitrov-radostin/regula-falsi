b83392ee27b9c796c03866315c7eb53185306ed7
Radostin Dimitrov <dimitrov.radostin@abv.bg>
2019-05-12 19:21:55 +0300

    mvp

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    console.log('hi');
     const canvas = document.getElementById('canvas')
     const context = canvas.getContext('2d')
     const PADDING = 35
@@ -10,10 +9,16 @@ document.addEventListener('DOMContentLoaded', ()=>{
     context.font = "18px Arial";
 
     const premadeFunctions = [
-        x => (Number(x) + 0.1) * (Number(x) - 0.3) - 0.3,
+        x => (x + 0.1) * (x - 0.3) - 0.3,
         x => Math.cos(x) - x,
         x => 0.5 - Math.sin(x),
     ]
+
+    let a, b // the borders of the interval, also used for drawing the lines
+    let values = []
+    let scaledValues = []
+    let f = () => {}
+    let scalingFactor = 1
 
     function drawFunction() {
         // Clear Canvas keeping previous state settings (font, color, trasnslations)
@@ -31,15 +36,15 @@ document.addEventListener('DOMContentLoaded', ()=>{
         context.lineWidth = 2
         context.stroke()
 
-        // Draw the function 
+        // Get values
         const selectedFunction = document.getElementById('selectedFunction').value
-        const f = premadeFunctions[selectedFunction]
+        f = premadeFunctions[selectedFunction]
         console.log(f , f(0));
-        const a = document.getElementById('intervalLeft').value
-        const b = document.getElementById('intervalRight').value
+        a = Number(document.getElementById('intervalLeft').value)
+        b = Number(document.getElementById('intervalRight').value)
 
         // Draw the function's graph
-        const values = []
+        values = []
         for (let i = 0; i < CANVAS_WIDTH; i++){
             values.push(f(a + (b - a) * i / CANVAS_WIDTH))
         }
@@ -50,9 +55,12 @@ document.addEventListener('DOMContentLoaded', ()=>{
             console.log('Your function is bad and you should feel bad');
             return 
         }
-        const scalingFactor = (CANVAS_HEIGHT / 2 ) / Math.max( maxValue, -minValue)
 
-        const scaledValues = values.map(v => v * scalingFactor)
+        //  move the avalidation elsewhere
+        scalingFactor = (CANVAS_HEIGHT / 2 ) / Math.max(maxValue, - minValue)
+        console.log(scalingFactor);
+
+        scaledValues = values.map(v => v * scalingFactor)
 
         for (let i = 0; i < CANVAS_WIDTH; i++){
             x = i
@@ -61,20 +69,20 @@ document.addEventListener('DOMContentLoaded', ()=>{
         }
 
         // Points and labels
-        {
-            const x = 0
-            const y = CANVAS_HEIGHT / 2 - scaledValues[0]
+        (() => {
+            const x1 = 0
+            const y1 = CANVAS_HEIGHT / 2 - scaledValues[0]
             const value = values[0].toFixed(2)
 
             context.beginPath()
-            context.arc(x, y, 4, 0, 2 * Math.PI )
+            context.arc(x1, y1, 4, 0, 2 * Math.PI )
             context.fill()
             context.fillText(
                 `(${a}, ${value})`,
-                x + 10 ,
-                value > 0 ? y - 10 : y + 30 
+                x1 + 10 ,
+                value > 0 ? y1 - 10 : y1 + 30 
             );
-        }
+        })()
         {
             const x = CANVAS_WIDTH
             const y = CANVAS_HEIGHT / 2 - scaledValues[scaledValues.length - 1]
@@ -84,14 +92,46 @@ document.addEventListener('DOMContentLoaded', ()=>{
             context.arc(x, y, 4, 0, 2 * Math.PI )
             context.fill()
             context.fillText(
-                `(${a}, ${value})`,
+                `(${b}, ${value})`,
                 x - 40 ,
                 value > 0 ? y - 10 : y + 30
             );
         }
     }
 
+    function drawNextLine () {
+        context.strokeStyle = '#AA0000'
+        context.lineWidth = 1
+        context.beginPath()
+        context.moveTo(a * CANVAS_WIDTH, CANVAS_HEIGHT / 2 - scalingFactor * f(a) )
+        context.lineTo(b * CANVAS_WIDTH, CANVAS_HEIGHT / 2 - scalingFactor * f(b))
+        context.stroke()
+
+        const intersectionWithAbscissa = (b * f(a) - a * f(b)) / (f(a) - f(b))
+        if (f(intersectionWithAbscissa) * f(a) < 0){
+            b = intersectionWithAbscissa
+        } else {
+            a = intersectionWithAbscissa
+        }
+        
+        context.beginPath()
+        context.moveTo(intersectionWithAbscissa * CANVAS_WIDTH, CANVAS_HEIGHT / 2 - f(intersectionWithAbscissa) * scalingFactor)
+        context.lineTo(intersectionWithAbscissa * CANVAS_WIDTH, CANVAS_HEIGHT / 2)
+        context.stroke()
+
+        return intersectionWithAbscissa
+        
+    }
+
     drawFunction()
+    // drawNextLine()
+
+    document.getElementById('getApproximation').addEventListener('click', event => {
+        let node = document.createElement('li')
+        node.textContent = drawNextLine()
+        document.getElementById('listOfApproximations').appendChild(node)
+    })
+
     document.addEventListener('input', event => {
         drawFunction()
     })
